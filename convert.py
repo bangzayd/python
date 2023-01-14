b518b977deb183589c4725a494e32ea4c9f48257
Ade Saputra <120370323+bangzayd@users.noreply.github.com>
2023-01-14 15:51:34 +0700

    Convert from exel

---
+++ convert.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import pandas as pd
+import numpy as np
+df = pd.read_excel('convert.xlsx')
+
+
+t1 = pd.to_datetime(df['b'], format='%H:%M:%S')
+t2 = pd.to_datetime(df['c'], format='%H:%M:%S')
+
+delta = t2 - t1
+
+print(delta)
+
+t3 = pd.to_datetime(df['x'], format='%H:%M:%S')
+t4 = pd.to_datetime(df['y'], format='%H:%M:%S')
+
+z = t4 - t3
+print(z)
